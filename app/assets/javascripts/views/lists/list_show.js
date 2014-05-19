b1b1da415e749ee2845a8776f1ee173fe8bf399a
zhyliana <zhyliana@gmail.com>
2014-05-18 23:38:45 -0700

    can add cards

--- app/assets/javascripts/views/lists/list_show.js
+++ app/assets/javascripts/views/lists/list_show.js
@@ -1,9 +1,15 @@
 Trellino.Views.ListShow = Backbone.CompositeView.extend({
   template: JST["lists/show"],
+  newCardTemplate: JST["cards/new"],
   className: "col-xs-4",
   
   initialize: function(){
     this.model.cards().each(this.addCard.bind(this));
+  },
+  
+  events: {
+    "click button#new-card" : "newCard",
+    "submit" : "submitNewCard",
   },
   
   addCard: function(card){
@@ -23,6 +29,32 @@ Trellino.Views.ListShow = Backbone.CompositeView.extend({
     this.attachSubviews();
     
     return this;
+  },
+  
+  
+  newCard: function(){ 
+    $(".new-card").html(this.newCardTemplate({
+      list: this.model
+    }));
+  },
+  
+  submitNewCard: function(event){
+    event.preventDefault();
+    
+    var params = $("form").serializeJSON().card;
+    var lastRank = this.model.cards().length;
+    var newCard = new Trellino.Models.Card(params);  
+    newCard.set({"rank": lastRank + 1});
+    var list = this.model;
+    
+    newCard.save({}, {
+      success: function(){ 
+        alert("success")
+        this.addCard(newCard);
+      }
+    });
+    
+    this.render();
   },
 })
 
